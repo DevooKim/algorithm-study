bd8c7324e448f7c8c47d4ed17dcc201588a11c7d
plzprayme <dbfpzk142@gmail.com>
2020-10-13 20:10:08 +0900

    책풀이

--- week2/book/7-5prayme.py
+++ week2/book/7-5prayme.py
@@ -20,5 +20,15 @@ class Solution:
         print(answer)
         return answer
 
+    def maxProfit2(self, prices: List[int]) -> int:
+        profit = 0
+        min_price = sys.maxsize
+
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+
+        return profit
+
 
 Solution().maxProfit([7, 1, 5, 3, 6, 4])
