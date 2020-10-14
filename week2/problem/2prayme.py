12f0945372d4b1d86d65392c23c4c0dd829b248a
plzprayme <dbfpzk142@gmail.com>
2020-10-14 10:35:55 +0900

    레벨2맞아?

--- week2/problem/2prayme.py
+++ week2/problem/2prayme.py
@@ -4,7 +4,42 @@ from typing import List
 # 쿼드압축 후 개수 세기
 class Solution:
     def solution(self, arr: List[List[int]]) -> int:
-        return answer
+        result = [0, 0]
+        n = len(arr)
+        split = n // 2
+
+        for i in range(0, len(arr), split):
+
+            # y축
+            for y in range(i+split-1, i-1, -1):
+                print(f'i: {i} y: {y}')
+
+            # x축
+            for x in range(i, i + split):
+                print(f"i: {i} x: {x}")
+
+        # def press(self, arr):
+        #     result = [0, 0]
+        #     n = len(arr)
+        #     split = n // 2
+        #
+        #     for i in range(0, len(arr), split):
+        #         for j in range(i, i+split):
+        #             print(f"i: {i} j: {j}")
+
+        # result = 0
+        # n = len(arr)
+        # split = n // 2
+        # # print(n // 2)
+        #
+        # for i in range(0, len(arr), split):
+        #     area = arr[i][i] + arr[i][i+1] + arr[i+1][i] + arr[i+1][i+1]
+        #     if area == n or area == 0:
+        #         result += area
+        #     else:
+
+        return 0
+
 
 
 Solution().solution(
