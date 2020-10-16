4e8075a360994dbffd28ba5d18c1653a9e0a9b2f
plzprayme <dbfpzk142@gmail.com>
2020-10-16 16:47:27 +0900

    Runner 풀이

--- week3/book/13prayme.py
+++ week3/book/13prayme.py
@@ -26,14 +26,29 @@ class Solution:
             array_list.append(head.val)
             head = head.next
 
-
         cnt = 0
         n = len(array_list) // 2
+        # while len(array_list) > 1:
         while cnt < n:
             if array_list.popleft() != array_list.pop():
                 return False
             cnt += 1
         return True
 
+    # 연결리스트에서는 반드씨 쓰이는 Runner
+    # fast 덕분에 slow는 무조건 연결 리스트의 중앙에 위치한다
+    def isPalindromeWithRunner(self, head: ListNode) -> bool:
+        rev = None
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next # runner를 활용해서 rev에 2개씩 넣기, slow 전진시키기
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev
 
 print(Solution().isPalindrome(ListNode(val=1, next=ListNode(val=2, next=None))))
