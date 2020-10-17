529a37eb15e628973a756a1116f83d3b1b68cc02
plzprayme <dbfpzk142@gmail.com>
2020-10-17 23:59:41 +0900

    15번 책 풀이

--- week3/book/15prayme.py
+++ week3/book/15prayme.py
@@ -22,23 +22,37 @@ class ListNode:
 
 
 class Solution:
-    pass
-def reverseList(self, head: ListNode) -> ListNode:
-    if not head:
-        return head
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
 
-    nodes = []
-    while head:
-        nodes.append(ListNode(head.val, None))
-        head = head.next
+        nodes = []
+        while head:
+            nodes.append(ListNode(head.val, None))
+            head = head.next
 
-    for i in range(len(nodes) - 1):
-        nodes[i + 1].next = nodes[i]
+        for i in range(len(nodes) - 1):
+            nodes[i + 1].next = nodes[i]
 
-    return nodes[-1]
+        return nodes[-1]
 
+    def bookRecurrent(self, head: ListNode) -> ListNode:
+        def reverse(node: ListNode, prev: ListNode = None):
+            if not node:
+                return prev
+            next, node.next = node.next, prev
+            return reverse(next, node)
 
+        return reverse(head)
 
+    def bookRepeat(self, head: ListNode) -> ListNode:
+        node, prev = head, None
+
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+
+        return prev
 
 
 
