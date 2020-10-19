e437165a78f03b739845e7e437ce0516f8046f75
DevooKim <khwoo8026@gmail.com>
2020-10-19 17:22:19 +0900

    풀었다.

---
+++ week3/book/15devoo.py
@@ -0,0 +1,22 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import *
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        result = None
+        while head:
+            result, result.next, head = head, result, head.next
+        return result
+
+a = Solution()
