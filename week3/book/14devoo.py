0cc8a19372552c69a02524fbe3a07895c40d359b
DevooKim <khwoo8026@gmail.com>
2020-10-19 17:13:44 +0900

    일단 풀었음

--- week3/book/14devoo.py
+++ week3/book/14devoo.py
@@ -14,25 +14,14 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        def append(self, result:ListNode, val) -> ListNode:
+        def append(result:ListNode, val) -> ListNode:
             result.next = ListNode(val)
             return result
         result = None
         while l1 and l2:
             if l1.val < l2.val:
-                #result, result.next, l1 = ListNode(l1.val), result, l1.next
-                if result is None:
-                    result = ListNode(l1.val)
-                else:
-                    result = ListNode(result.val, append(result, l1.val))
-                l1 = l1.next
+                result, result.next, l1 = ListNode(l1.val), result, l1.next
             else:
-                #result, result.next, l2 = ListNode(l2.val), result, l2.next
-                if result is None:
-                    result = ListNode(l2.val)
-                else:
-                    result = append(result, l2.val)
-                
-                l2 = l2.next
+                result, result.next, l2 = ListNode(l2.val), result, l2.next
                 
         print(result)
