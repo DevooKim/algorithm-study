74b5a9e27c6f1655106e65526751c046f9c16c50
DevooKim <khwoo8026@gmail.com>
2020-10-20 09:05:30 +0900

    풀었다

--- week3/book/16devoo.py
+++ week3/book/16devoo.py
@@ -14,29 +14,33 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        def toList(arr: List, l: ListNode):
+            arr.append(l.val)
+            l = l.next
+            return arr, l
+
+        def toNum(arr: List) -> int:
+            n = 0
+            c = 10 ** (len(arr) - 1)
+            for i in arr[::-1]:
+                n += i * c
+                c //= 10
+            return n
+
         a1, a2 = [], []
         n1, n2 = 0, 0
+        result = None
+        
         while l1:
-            a1.append(l1.val)
-            l1 = l1.next
+            a1, l1 = toList(a1, l1)
         while l2:
-            a2.append(l2.val)
-            l2 = l2.next
-        
-        c = 10 ** (len(a1) - 1)
-        for i in a1[::-1]:
-            n1 += i * c
-            c /= 10
-        
-        c = 10 ** (len(a2) - 1)
-        for i in a2[::-1]:
-            n2 += i * c
-            c /= 10
-            
-        return n1 + n2
+            a2, l2 = toList(a2, l2)
 
-
-
-            
+        n1 = toNum(a1)
+        n2 = toNum(a2)
+        s = str(n1 + n2)
+        for i in range(len(s)):
+            result, result.next = ListNode(s[i]), result
+        return result
 
 a = Solution()
