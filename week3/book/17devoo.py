948baec0f13a9a4f2751cea9cb734578c8ca67f0
DevooKim <khwoo8026@gmail.com>
2020-10-20 10:26:45 +0900

    재귀..

--- week3/book/17devoo.py
+++ week3/book/17devoo.py
@@ -20,4 +20,27 @@ class Solution:
             swp.val, swp.next.val = swp.next.val, swp.val
             swp = swp.next.next
         return head
-a = Solution()
+
+    def swap(self, head: ListNode) -> ListNode:
+        root = prev = ListNode(None)
+        prev.next = head
+
+        while head and head.next:
+            b = head.next
+            head.next = b.next
+            b.next = head
+
+            prev.next = b
+
+            head = head.next
+            prev = prev.next.next
+
+        return root.next
+
+    def Recursion(self, head: ListNode) -> ListNode:
+        if head and head.next:
+            p = head.next
+            head.next = self.Recursion(p.next)
+            p.next = head
+            return p
+        return head
