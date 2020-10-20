319b724b5d1a932fd6a8f43933d706c0b7adfe1d
DevooKim <khwoo8026@gmail.com>
2020-10-20 16:44:53 +0900

    모르겠다

--- week3/problem/2346devoo.py
+++ week3/problem/2346devoo.py
@@ -1,16 +1,15 @@
 n = int(input())
 l = list(map(int, input().split()))
 
-i = -1
+i = 0
 tmp = l
 p = []
 for _ in range(len(l)):
-    i += 1
     p.append(tmp[i])
-    print(tmp[i])
     tmp.pop(i)
-    if tmp[i] < 0:
-        i = (i - tmp[i]) % len(tmp)
+    
+    if tmp[i] > 0:
+        i = (i + tmp[i]) % len(tmp)
     else:
-        i = (i + tmp[i] - 1) % len(tmp)
+        i = abs(i + tmp[i]) % len(tmp)
 print(p)
