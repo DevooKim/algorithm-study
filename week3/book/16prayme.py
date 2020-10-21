568aedbe48cebc58e66791d5cc3f31e17be4c7f6
plzprayme <dbfpzk142@gmail.com>
2020-10-21 16:49:47 +0900

    다시 풀었음

--- week3/book/16prayme.py
+++ week3/book/16prayme.py
@@ -23,6 +23,45 @@ class ListNode:
 
 class Solution:
     def reverseList(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # list1, list2 = [], []
+        # while l1 or l2:
+        #     if l1:
+        #         list1.append(l1.val)
+        #         l1 = l1.next
+        #
+        #     if l2:
+        #         list2.append(l2.val)
+        #         l2 = l2.next
+        #
+        #
+        # num1, num2 = 0, 0
+        # square1, square2 = len(list1) - 1, len(list2) - 1
+        # while list1 or list2:
+        #     if list1:
+        #         num1 += list1.pop() * 10 ** square1
+        #         square1 -= 1
+        #
+        #     if list2:
+        #         num2 += list2.pop() * 10 ** square2
+        #         square2 -= 1
+        #
+        # answer = []
+        # result = num1 + num2
+        # # while result > 0:
+        # #     answer.append(result % 10)
+        # #     result //= 10
+        #
+        # # print(answer)
+        #
+        # def get(val, tail=None):
+        #     if val // 10 > 0:
+        #         b = ListNode(val % 10, tail)
+        #         return get(val // 10, b)
+        #     return ListNode(val, tail)
+
+
+        # return get(result).toString()
+
         list1, list2 = [], []
         while l1 or l2:
             if l1:
@@ -32,7 +71,6 @@ class Solution:
             if l2:
                 list2.append(l2.val)
                 l2 = l2.next
-
 
         num1, num2 = 0, 0
         square1, square2 = len(list1) - 1, len(list2) - 1
@@ -45,21 +83,21 @@ class Solution:
                 num2 += list2.pop() * 10 ** square2
                 square2 -= 1
 
-        answer = []
         result = num1 + num2
         # while result > 0:
         #     answer.append(result % 10)
         #     result //= 10
 
-        # print(answer)
+        # str_result = str(result)
 
-        def get(val, tail=None):
-            if val // 10 > 0:
-                b = ListNode(val % 10, tail)
-                return get(val // 10, b)
-            return ListNode(val, tail)
+        result = str(result)
+        a = ListNode(result[0], None)
+        b = ListNode(None, None)
+        for i in result[1:]:
+            b = ListNode(i, a)
+            a = b
 
-        return get(result).toString()
+        return a
 
 
 Solution().reverseList(
