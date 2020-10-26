d5188711491eb3838cb94099c83ecd0e9100e78a
DevooKim <khwoo8026@gmail.com>
2020-10-26 10:58:30 +0900

    이해 불가

--- week4/book/21devoo.py
+++ week4/book/21devoo.py
@@ -10,18 +10,27 @@ from typing import *
 
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        stack = []
-        s = sorted(s)
-        result = ""
-        for c in s:
-            if not stack:
-                stack.append(c)
-            else:
-                if stack[-1] != c:
-                    result += stack.pop()
-                    stack.append(c)
-        result += stack.pop()
-        return result
+        print(f'set(s): {set(s)}')
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            print(suffix)
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
+        return ''
+
+    def stackFunc(self, s: str) -> str:
+        counter, seen, stack = collections.Counter(s), set(), []
+
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
+
+        return ''.join(stack)
 
 a = Solution()
 print(a.removeDuplicateLetters("cbacdcbc"))
