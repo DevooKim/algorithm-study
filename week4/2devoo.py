a1bbfd521dacb2b011c52157742979c27c9a900b
DevooKim <khwoo8026@gmail.com>
2020-10-27 11:22:00 +0900

    통과

--- week4/2devoo.py
+++ week4/2devoo.py
@@ -4,12 +4,12 @@ def solution(progresses, speeds):
     answer, stack = [], []
     progresses = list(map(lambda x: 100 - x, progresses))
     progresses = [math.ceil(p / s) for p, s in zip(progresses, speeds)]
+    print(f'해아할 작업일 수: {progresses}')
 
     #빗물 트래킹, 주식 방법
     #자신 이후에 낮은 것들을 같이 빼버린다.
-
     for cur in progresses:
-        while stack and cur > stack[-1]:
+        while stack and cur > stack[0]:
             answer.append(len(stack))
             stack = []
         stack.append(cur)
@@ -19,3 +19,4 @@ def solution(progresses, speeds):
 
 print(solution([93, 30, 55], [1, 30, 5]))
 print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
+#print(solution([0,3,3,3,3],[1,2,2,1,2,2]))
