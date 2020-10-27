fb3b01a72cba0e14a60eca10a5039507222571df
plzprayme <dbfpzk142@gmail.com>
2020-10-27 16:59:32 +0900

    책 풀이

--- week4/book/20prayme.py
+++ week4/book/20prayme.py
@@ -5,6 +5,7 @@ class Solution:
         # ( = 40, ) = 41
         # [ = 91, ] = 93
         # { = 123, } = 125
+        # 우선순위를 알아야한다.
 
         small, medium, large = 0, 0, 0
         for i in s:
@@ -23,10 +24,25 @@ class Solution:
 
             if small < 0 or medium < 0 or large < 0:
                 return False
-            elif small + medium + large > 1:
-                return False
+
         return True
 
 
+    def book(self, s: str) -> bool:
+        stack = []
+        table = {
+            ')':'(',
+            '}':'{',
+            ']':'[',
+        }
+
+        for char in s:
+            if char not in table: # table.keys()에 없으면 즉, 여는 괄호일 떄
+                stack.append(char) # stack에 추가
+            elif not stack or table[char] != stack.pop(): # stack이 비어있지 않거나 닫힌 괄호가 없으면..
+                return False
+        return len(stack) == 0
+
 for i in ["()", "()[]{}", "(]", "([)]", "{[]}"]:
-    print(Solution().isValid(i))
+    # print(Solution().isValid(i))
+    print(Solution().book(i))
