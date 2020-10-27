3a37eecf299ee60b532d129a3e9e4e1cafef2a5e
DevooKim <khwoo8026@gmail.com>
2020-10-27 09:28:05 +0900

    시간비교

--- week4/book/22devoo.py
+++ week4/book/22devoo.py
@@ -7,9 +7,11 @@ import sys
 import math
 import bisect
 from typing import *
+import time
 
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
+        start = time.time()
         result = []
         for i, t in enumerate(T):
             L = T[i:]
@@ -22,18 +24,26 @@ class Solution:
             if count == len(L):
                 count = 0
             result.append(count)
+        print(f'my: {time.time() - start}')
         return result
 
     def stackSolution(self, T: List[int]) -> List[int]:
+        start = time.time()
         answer = [0] * len(T)
         stack = []
         for i, cur in enumerate(T):
             while stack and cur > T[stack[-1]]:
+                print(stack, answer)
                 last = stack.pop()
                 answer[last] = i - last
             stack.append(i)
-
+        print(f'book: {time.time() - start}')
         return answer
 
 a = Solution()
-print(a.dailyTemperatures([55,38,53,81,61,93,97,32,43,78]))
+
+#print(a.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
+print(a.stackSolution([73, 74, 75, 71, 69, 72, 76, 73]))
+
+#my: 1.4972984790802002
+#book: 0.00183868408203125
