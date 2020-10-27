06ad67b54bca0b130dc8ab3183ada3cd51b3dfc0
plzprayme <dbfpzk142@gmail.com>
2020-10-27 18:48:12 +0900

    checkpoint

---
+++ week4/book/21prayme.py
@@ -0,0 +1,24 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import List
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+
+
+
+
+for i in ["bcabc", "cbacdcbc"]:
+    print(Solution().removeDuplicateLetters(i))
+
+
+
+
+
+
