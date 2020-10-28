c34e2b5b9e2225e0ebc438b2f647829fab098fc6
DevooKim <khwoo8026@gmail.com>
2020-10-28 17:40:42 +0900

    .

--- week4/problem/1devoo.py
+++ week4/problem/1devoo.py
@@ -6,17 +6,20 @@ def solution(prices):
     start = time.time()
     answer = []
     prices = deque(prices)
+    count = 0
 
     while prices:
         price = prices.popleft()
         days = 0
         for other_price in prices:
+            count += 1
             days += 1
             if price > other_price:
                 break
         answer.append(days)
 
     answer[-1] = 0
+    print(count)
     print(f'solution: {time.time() - start}')
     return answer
 
@@ -35,8 +38,9 @@ def solution2(prices):
     #남은 스택 처리
     for s in stack:
         answer[s] = len(prices) - 1 - s
+    print(count)
     print(f'solution2: {time.time() - start}')
     return answer
 
-print(solution([1,2,3,2,3,1,2,3,2,3,1,2,3,2,3,1,2,3,2,3,1,2,3,2,3]))
+#print(solution([1,2,3,2,3,1,2,3,2,3,1,2,3,2,3,1,2,3,2,3,1,2,3,2,3]))
 print(solution2([1,2,3,2,3,1,2,3,2,3,1,2,3,2,3,1,2,3,2,3,1,2,3,2,3]))
--- week4/problem/2devoo.py
+++ week4/problem/2devoo.py
@@ -3,20 +3,21 @@ import time
 def solution(progresses, speeds):
     start = time.time()
     answer, stack = [], []
-    #progresses = list(map(lambda x: 100 - x, progresses))
-    #progresses = [math.ceil(p / s) for p, s in zip(progresses, speeds)]
+    progresses = list(map(lambda x: 100 - x, progresses))
+    progresses = [math.ceil(p / s) for p, s in zip(progresses, speeds)]
 
     #ceil을 안쓰고 이런식으로도 가능하다....
     #ceil을 안쓰는 것이 아주 근소하게 더 빠르다. (아래 첫번째 케이스일 경우 평균 1ms)
     #다시 해보니 실행할 때 마다 결과의 차이가 커서 비교불가
-    progresses = list(map(lambda x: x - 100, progresses))
-    progresses = [-(p // s) for p, s in zip(progresses, speeds)]
+    #progresses = list(map(lambda x: x - 100, progresses))
+    #progresses = [-(p // s) for p, s in zip(progresses, speeds)]
     print(f'남은 작업일 수: {progresses}')
 
     #빗물 트래킹, 주식 방법
     #자신 이후에 낮은 것들을 같이 빼버린다.
     for cur in progresses:
         #cur보다 작은 것들을 cur보다 큰 수가 나올 때 까지 stack에 쌓는다.
+        print(stack)
         while stack and cur > stack[0]:
             answer.append(len(stack))
             stack = []
@@ -25,6 +26,6 @@ def solution(progresses, speeds):
     print(time.time() - start)
     return answer
 
-print(solution([93, 30, 55], [1, 30, 5]))
+#print(solution([93, 30, 55], [1, 30, 5]))
 print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
 #print(solution([0,3,3,3,3],[1,2,2,1,2,2]))
