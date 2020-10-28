15595adb5ac6b32d98880165cc7d79e6efb639c0
plzprayme <dbfpzk142@gmail.com>
2020-10-28 13:08:40 +0900

    rnd 눈물의 동궈쇼 성공..

--- week4/problem/stock_prayme.py
+++ week4/problem/stock_prayme.py
@@ -1,20 +1,16 @@
+import collections
+
 def solution(prices):
     answer = [0] * len(prices)
 
-    front, back = 0, 1
-    cnt = 0
-    while front <= len(prices) - 2 and back <= len(prices) - 1:
-        cnt += 1
-        if prices[front] > prices[back]:
-            answer[front], front, back = back - front, front + 1, front + 2
-            continue
+    deq = collections.deque()
 
-        if front != len(prices) - 1 and back == len(prices) - 1:
-            answer[front] = len(prices) - (front + 1)
-            front, back = front + 1, front + 2
-            continue
-        back += 1
-    print(cnt)
+    for idx, cur in enumerate(prices):
+
+        if cur > deq[-1]:
+
+
+        deq.append(idx)
+
     return answer
-
 solution([1,2,3,2,3])
