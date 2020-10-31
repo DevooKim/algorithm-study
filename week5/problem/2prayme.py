6da3f71f348afb8df1a6b67fc3d375a57665b428
plzprayme <dbfpzk142@gmail.com>
2020-10-31 15:49:33 +0900

    2번 문제 똥꼬쇼

--- week5/problem/2prayme.py
+++ week5/problem/2prayme.py
@@ -1,16 +1,26 @@
 def solution(encrypted_text, key, rotation):
     table = 'abcdefghijklmnopqrstuvwxyz'
 
-    for i in range(rotation):
-        encrypted_text = encrypted_text[1:] + encrypted_text[0]
+    if len(encrypted_text) == 1:
+        weight = table.index(key) + 1
+        return table[(table.index(encrypted_text[i]) - weight)]
+    elif len(encrypted_text) == '':
+        return ''
 
-    # unrotation = encrypted_text[rotation:] + encrypted_text[:rotation]
-    #
-    # answer = ''
-    # for i, k in enumerate(key):
-    #     weight = table.index(k) + 1
-    #     answer += table[(table.index(unrotation[i]) - weight)]
-    # return answer
+    if rotation > 0:
+        for i in range(rotation):
+            encrypted_text = encrypted_text[1:] + encrypted_text[0]
+    elif rotation < 0:
+        for i in range(-rotation):
+            encrypted_text = encrypted_text[0] + encrypted_text[1:]
+
+    unrotation = encrypted_text
+    answer = ''
+    for i, k in enumerate(key):
+        weight = table.index(k) + 1
+        answer += table[(table.index(unrotation[i]) - weight)]
+
+    return answer
 
 # solution('hellopython','abcdefghijk',3)
-solution('qyyigoptvfb','abcdefghijk',3)
+solution('qyyigoptvfb','abcdefghijk',-3)
