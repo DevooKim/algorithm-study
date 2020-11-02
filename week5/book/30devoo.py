afbde813e51121bd46d4e1d293a80a203f3543ba
DevooKim <khwoo8026@gmail.com>
2020-11-02 10:12:13 +0900

    내풀이

---
+++ week5/book/30devoo.py
@@ -0,0 +1,26 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import *
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        _max = -sys.maxsize
+        for i in range(len(s)):
+            freq = {}
+            for char in s[i:]:
+                if char not in freq:
+                    freq[char] = 1
+                else:
+                    break
+            _max = max(_max, len(freq))
+        return _max                
+
+a = Solution()
+print(a.lengthOfLongestSubstring("abcabcbb"))
+print(a.lengthOfLongestSubstring("bbbbb"))
