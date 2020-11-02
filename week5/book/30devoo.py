bddb36d2174d6fa25ed0ffcbb079c61d7f9e143f
DevooKim <khwoo8026@gmail.com>
2020-11-02 10:17:58 +0900

    책풀이

--- week5/book/30devoo.py
+++ week5/book/30devoo.py
@@ -21,7 +21,20 @@ class Solution:
                 else:
                     break
             _max = max(_max, len(freq))
-        return _max                
+        return _max
+
+    def twoPointer(self, s: str) -> int:
+        used = {}
+        max_len = start = 0
+
+        for index, char in enumerate(s):
+            if char in used and start <= used[char]:
+                start = used[char] + 1
+            else:
+                max_len = max(max_len, index - start + 1)
+            used[char] = index
+
+        return max_len
 
 a = Solution()
 print(a.lengthOfLongestSubstring("abcabcbb"))
