a782eb902f408b9bfa0474782720d956153dfa99
DevooKim <khwoo8026@gmail.com>
2020-11-02 09:49:47 +0900

    내풀이 수정

--- week5/book/29devoo.py
+++ week5/book/29devoo.py
@@ -11,15 +11,28 @@ from typing import *
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         result = 0
-        p = re.compile('\w')
-        J = p.findall(J)
-        S = p.findall(S)
 
         for j in J:
             result += S.count(j)
         return result
+
+    def hashTable(self, J: str, S: str) -> int:
+        h = {}
+        count = 0
+
+        for s in S:
+            if s not in h:
+                h[s] = 1
+            else:
+                h[s] += 1
+
+        for j in J:
+            if j in h:
+                count += h[j]
+        return count
         
 
 
 a = Solution()
 print(a.numJewelsInStones("aA", "aAABBBB"))
+print(a.hashTable("aA", "aAABBBB"))
