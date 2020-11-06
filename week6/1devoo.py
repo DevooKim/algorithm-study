e150ccccc32af3896d954aee698580e77929816b
DevooKim <khwoo8026@gmail.com>
2020-11-06 11:15:19 +0900

    개꿀

---
+++ week6/1devoo.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    answer = 0
+    for i, j in zip(a,b):
+        answer += (i * j)
+    return answer
+
+print(solution([1,2,3,4], [-3,-1,0,2]))
+print(solution([-1,0,1], [1,0,-1]))
