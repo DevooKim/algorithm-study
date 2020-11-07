e92060a60eb9684a33940cbd42ca47b3c5f30f61
plzprayme <dbfpzk142@gmail.com>
2020-11-07 23:54:35 +0900

    32번 책코드 안되네?

--- week6/book/32prayme.py
+++ week6/book/32prayme.py
@@ -46,7 +46,27 @@ class Solution:
 
         return answer
 
-    # def book(self, grid: List[List[str]]) -> int:
+    def book(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or \
+                    j < 0 or j >= len(grid[0]) or \
+                    grid[i][j] != '1':
+                return
+
+            grid[i][j] = 0
+
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    dfs(i, j)
+                    count += 1
+        return count
 
 
 # grid의 행과 열 단위로 육지(1)인 곳을 찾아 진행한다.
