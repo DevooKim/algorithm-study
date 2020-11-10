80f932e3888b4ae8259a959ea94df56243e48356
DevooKim <khwoo8026@gmail.com>
2020-11-10 11:20:56 +0900

    해결

---
+++ week6/book/37devoo.py
@@ -0,0 +1,31 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import *
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        prev = []
+
+        def dfs(elements):
+            if len(prev[:]) > len(nums):
+                return
+            result.append(prev[:])
+
+            for idx, e in enumerate(elements):
+                prev.append(e)
+                dfs(elements[idx+1:])
+                prev.pop()
+
+        dfs(nums)
+        return result
+                
+
+a = Solution()
+print(a.subsets([1,2,3]))
