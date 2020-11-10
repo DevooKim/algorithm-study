08461c4eb5b67d9b28bd0ca157e607fe9310c6f5
plzprayme <dbfpzk142@gmail.com>
2020-11-11 00:38:40 +0900

    프로그래머스 3 잠시

---
+++ week6/problem/3prayme.py
@@ -0,0 +1,21 @@
+def solution(a):
+
+    # 스타수열의 조건
+    # 길이가 2이상의 짝수 (빈수열 X)
+    # 같은 길이끼리의 부분 수열은 교집합인 원소가 1개 이상있어야한다.
+    # 인접하는 원소는 같으면 안된다.
+
+    max_length, tmp = 0, 1
+    while tmp > a:
+        a
+
+
+    print(max_length)
+
+    answer = -1
+    return None
+
+
+print(solution([0]))
+print(solution([5,2,3,3,5,3]))
+print(solution([0,3,3,0,7,2,0,2,2,0]))
