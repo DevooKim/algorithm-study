918a4064aa2cbf25809be39f0460ba37cc911c5a
DevooKim <khwoo8026@gmail.com>
2020-11-10 09:55:20 +0900

    지렸다

--- week6/book/36devoo.py
+++ week6/book/36devoo.py
@@ -19,14 +19,24 @@ class Solution:
             elif _sum > target:
                 return
             
-            for e in elements:
-                next = elements[:]
+            # for e in elements:
+            #     prev.append(e)
+            #     dfs(_sum + e, elements)
+            #     prev.pop()
+
+            for idx, e in enumerate(elements):
                 prev.append(e)
-                dfs(_sum + e, next)
+                if idx > 0:
+                    dfs(_sum + e, elements[idx:])
+                else:
+                    dfs(_sum + e, elements[:])
                 prev.pop()
+                
+
                 
         dfs(0, candidates)
         return result
 a = Solution()
 
 print(a.combinationSum([2,3,6,7], 7))
+print(a.combinationSum([2,3,5], 8))
