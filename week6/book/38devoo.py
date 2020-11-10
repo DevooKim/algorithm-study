bd370261182e0f17e72bff13e9f4ad88c0064ea8
DevooKim <khwoo8026@gmail.com>
2020-11-10 13:26:34 +0900

    책풀이 추가

--- week6/book/38devoo.py
+++ week6/book/38devoo.py
@@ -43,8 +43,27 @@ class Solution:
                 break
         return path
 
+    def book(self, tickets: List[List[str]]) -> List[str]:
+        graph = collections.defaultdict(list)
+
+        for a, b in sorted(tickets):
+            graph[a].append(b)
+
+        print(graph)
+        route = []
+        def dfs(a):
+            while graph[a]:
+                dfs(graph[a].pop(0))
+            route.append(a)
+
+        dfs("JFK")
+        return route[::-1]
+    
+
 a = Solution()
-print(a.findItinerary([["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]))
-print(a.findItinerary([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]))
-print(a.findItinerary([["JFK","SFO"]]))
-print(a.findItinerary([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
+#print(a.findItinerary([["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]))
+print(a.book([["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]))
+# print(a.findItinerary([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]))
+# print(a.findItinerary([["JFK","SFO"]]))
+# print(a.findItinerary([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
+# print(a.book([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
