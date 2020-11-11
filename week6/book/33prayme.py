5ef3355f4b1f36716a182906aa41e524e3d1dcd4
plzprayme <dbfpzk142@gmail.com>
2020-11-11 13:06:46 +0900

    33번 다시 풀음

--- week6/book/33prayme.py
+++ week6/book/33prayme.py
@@ -11,7 +11,9 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        count = [0]
         def dfs(index, path):
+            count[0] += 1
             if len(path) == len(digits):
                 result.append(path)
                 return
@@ -36,10 +38,38 @@ class Solution:
 
         result = []
         dfs(0, "")
+        print(f'COUNT: {count[0]}')
+        return result
 
+    def retry(self, digits: str) -> List[str]:
+        count = [0]
+        letters = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+
+
+
+        result = []
+        def dfs(letter, num):
+            count[0] += 1
+            if len(num) == len(digits):
+                result.append(num)
+                return
+
+            for char in letters[letter[0]]:
+                dfs(letter[1:], num + char)
+
+        dfs(digits, "")
+        print(f'COUNT: {count[0]}')
         return result
 
 
+print(Solution().retry("23"))
+Solution().retry("23456789")
+# print(Solution().retry(""))
+# print(Solution().retry("2"))
 print(Solution().letterCombinations("23"))
-print(Solution().letterCombinations(""))
-print(Solution().letterCombinations("2"))
+Solution().letterCombinations("23456789")
+# print(Solution().letterCombinations(""))
+# print(Solution().letterCombinations("2"))
