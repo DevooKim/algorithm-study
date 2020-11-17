65788fa6485d9d24718f821949d3ec26a07c6fd6
DevooKim <khwoo8026@gmail.com>
2020-11-17 15:06:21 +0900

    책풀이 추가

--- week7/book/52devoo.py
+++ week7/book/52devoo.py
@@ -8,6 +8,8 @@ import math
 import bisect
 from typing import *
 
+#풀었음 => 브루드포스
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -29,4 +31,45 @@ class Solution:
         recursive(root)
         return self.result
 
+    def book(self, root: TreeNode, low: int, high: int) -> int:
+        def dfs(node: TreeNode):
+            if not node:
+                return 0
+
+            if node.val < low:
+                return dfs(node.right)
+            elif node.val > high:
+                return dfs(node.left)
+
+            return node.val + dfs(node.left) + dfs(node.right)
+
+    def book2(self, root: TreeNode, low: int, high: int) -> int:
+        stack, sum = [root], 0
+
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val > low:
+                    stack.append(node.left)
+                elif node.val < high:
+                    stack.append(node.right)
+                
+                if low <= node.val <= high:
+                    sum += node.val
+        return sum
+
+    def book3(self, root: TreeNode, low: int, high: int) -> int:
+        stack, sum = [root], 0
+
+        while stack:
+            node = stack.pop(0)
+            if node:
+                if node.val > low:
+                    stack.append(node.left)
+                elif node.val < high:
+                    stack.append(node.right)
+                
+                if low <= node.val <= high:
+                    sum += node.val
+        return sum
 a = Solution()
