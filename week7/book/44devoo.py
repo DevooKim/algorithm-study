122cdb25dcda8d8aeb67be86626418316b955ca8
DevooKim <khwoo8026@gmail.com>
2020-11-17 10:05:43 +0900

    책풀이 추가, 어렵다

--- week7/book/44devoo.py
+++ week7/book/44devoo.py
@@ -8,6 +8,7 @@ import math
 import bisect
 from typing import *
 
+#못풀음
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -42,5 +43,30 @@ class Solution:
 
         dfs(root)
         return max(self.depth)
+
+    result: int = 0
+    def treeDFS(self, root: TreeNode) -> int:
+        def dfs(node: TreeNode):
+            if node is None:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if node.left and node.left.val == node.val:
+                left += 1
+            else:
+                left = 0
+
+            if node.right and node.right.val == node.val:
+                right += 1
+            else:
+                right = 0
+
+            self.result = max(self.result, left+right)
+            return max(left, right)
+
+        dfs(root)
+        return self.result
 
 a = Solution()
