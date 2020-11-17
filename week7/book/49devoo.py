48c488b28018dcccddb1db22673f36b3aa19a4a9
DevooKim <khwoo8026@gmail.com>
2020-11-17 14:00:31 +0900

    책풀이 추가

--- week7/book/49devoo.py
+++ week7/book/49devoo.py
@@ -38,8 +38,37 @@ class Solution:
         for k in graph.keys():
             dfs(graph, k, [])
         return self.result[1]
-                    
 
+
+    def book(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n <= 1:
+            return [0]
+
+        graph = collections.defaultdict(list)
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+
+        leaves = []
+
+        #첫번째 리프노드 추가
+        for i in range(n+1):
+            if len(graph[i]) == 1:
+                leaves.append(i)
+
+        while n > 2:
+            n -= len(leaves)
+            new_leaves = []
+
+            for leaf in leaves:
+                neighbor = graph[leaf].pop()
+                graph[neighbor].remove(leaf)
+
+                if len(graph[neighbor]) == 1:
+                    new_leaves.append(neighbor)
+            leaves = new_leaves
+        return leaves
+    
 a = Solution()
 print(a.findMinHeightTrees(4, [[1,0],[1,2],[1,3]]))
 print(a.findMinHeightTrees(6, [[3,0],[3,1],[3,2],[3,4],[5,4]]))
