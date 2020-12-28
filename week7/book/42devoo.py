5505089a3c724e2772cb9279680a76ad593d776b
DevooKim <khwoo8026@gmail.com>
2020-12-28 11:24:30 +0900

    책풀이 추가

--- week7/book/42devoo.py
+++ week7/book/42devoo.py
@@ -32,5 +32,24 @@ class Solution:
 
     rec(root, 0)
     return max(result) + 1
+
+  def book(self, root: TreeNode) -> int:
+    if root is None:
+      return 0
+
+    queue = collections.deque([root])
+    depth = 0
+
+    while queue:
+      depth += 1
+      for _ in range(len(queue)):
+        cur_root = queue.popleft()
+        if cur_root.left:
+          queue.append(cur_root.left)
+
+        if cur_root.right:
+          queue.append(cur_root.right)
+
+    return depth
     
 a = Solution()
