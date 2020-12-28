f3b6a7d5236cb0ff11f4a3fe62aa9a3ecca15aba
DevooKim <khwoo8026@gmail.com>
2020-12-28 10:09:15 +0900

    다익스트라 책풀이

---
+++ week7/book/41devoo.py
@@ -0,0 +1,15 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import *
+
+class Solution:
+  def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+      
+
+a = Solution()
