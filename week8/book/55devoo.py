ef8cd36724885839bf8e2736c19e79cc1a89f8af
DevooKim <khwoo8026@gmail.com>
2021-01-04 09:41:09 +0900

    55번 책풀이 추가

--- week8/book/55devoo.py
+++ week8/book/55devoo.py
@@ -18,6 +18,23 @@ class Solution:
 
         return result
 
+    def book1(self, nums, k):
+        heap = list()
+        for n in nums:
+            heapq.heappush(heap, -n)
+        for _ in range(k):
+            heapq.heappop(heap)
+
+        return heapq.heappop(heap)
+
+    def book2(self, nums, k):
+        return heapq.nlargest(k, nums)[-1]
+
+    def book3(self, nums, k):
+        return sorted(nums, reverse=True)[k-1]
+
+    
+
 a = Solution()
 
 print(a.findKthLargest([3,2,1,5,6,4], 2))
