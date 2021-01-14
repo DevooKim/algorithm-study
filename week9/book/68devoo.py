d44896af906bc4b6bd32842136e5fd90a0157efe
DevooKim <khwoo8026@gmail.com>
2021-01-14 11:39:02 +0900

    책풀이 추가

--- week9/book/68devoo.py
+++ week9/book/68devoo.py
@@ -22,6 +22,35 @@ class Solution:
             else:
                 return [left+1, right+1]
 
+    def book1(self, numbers, target):
+        for k, v in enumerate(numbers):
+            left, right = k + 1, len(numbers) - 1
+            expected = target - v
+
+            while left <= right:
+                mid = left + (right - left) // 2
+                if numbers[mid] < expected:
+                    left = mid + 1
+                elif numbers[mid] > expected:
+                    right = mid - 1
+                else:
+                    return k + 1 ,mid + 1
+
+    def book2(self, numbers, target):
+        for k, v in enumerate(numbers):
+            expected = target - v
+            nums = numbers[k + 1:]
+            i = bisect.bisect_left(nums, expected)
+            if i < len(nums) and  nums == expected:
+                return k + 1, i + k + 2
+
+    def book3(self, numbers, target):
+        for k, v in enumerate(numbers):
+            expected = target - v
+            i = bisect.bisect_left(numbers, expected, k + 1)
+            if i < len(numbers) and numbers[i] == expected:
+                return k + 1, i + 1
+
 
                 
 
