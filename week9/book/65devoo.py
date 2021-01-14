561074684dae51d95f8ca24b46fa03aa0ae64fa2
DevooKim <khwoo8026@gmail.com>
2021-01-14 09:38:08 +0900

    책을 또 이겨버렸습니다.

--- week9/book/65devoo.py
+++ week9/book/65devoo.py
@@ -8,6 +8,7 @@ import math
 import bisect
 from typing import *
 
+#풀었음
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         def bs(arr, offset):
@@ -27,6 +28,22 @@ class Solution:
         if nums[0] == target:
             return 0
         return bs(nums, 0)
+
+
+    def book(self, nums, target):
+        def binary_search(left, right):
+            if left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] < target:
+                    return binary_search(mid + 1, right)
+                elif nums[mid] > target:
+                    return binary_search(left, mid - 1)
+                else:
+                    return mid
+
+            else:
+                return -1
 
 a = Solution()
 print(a.search([-1,0,3,5,9,12], 9))
