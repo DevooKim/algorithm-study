c8a275730584be296e3e4351cb51e4687e35f2f2
DevooKim <khwoo8026@gmail.com>
2021-01-14 10:34:41 +0900

    책풀이 추가

--- week9/book/66devoo.py
+++ week9/book/66devoo.py
@@ -8,6 +8,7 @@ import math
 import bisect
 from typing import *
 
+#풀었는데 맞는건가
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         result = [-1]
@@ -29,6 +30,36 @@ class Solution:
 
         bs(nums, 0)
         return result[0]
+
+    def book(self, nums, target):
+        if not nums:
+            return -1
+
+        #최솟값 찾기
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        pivot = left
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_pivot = (mid + pivot) % len(nums)
+
+            if nums[mid_pivot] < target:
+                left = mid + 1
+            elif nums[mid_pivot] > target:
+                right = mid - 1
+            else:
+                return mid_pivot
+
+        return -1
         
 a = Solution()  
 print(a.search([4,5,6,7,0,1,2], 1))
