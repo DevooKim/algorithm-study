0d3d10d0ea8cec6c8026f94e1543c785b09caeb4
plzprayme <dbfpzk142@gmail.com>
2021-01-15 16:42:45 +0900

    74번

---
+++ week9/book/74prayme.py
@@ -0,0 +1,17 @@
+import collections
+import heapq
+import functools
+import itertools
+import re
+import sys
+import math
+import bisect
+from typing import List
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        print(n & sys.maxsize)
+    
+    def book(self, n: int) -> int:
+        # return bin(n ^ 0).count('1')
+        return bin(n).count('1')
