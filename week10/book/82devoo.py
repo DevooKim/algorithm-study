143a3a9b42c5156b52911fcb1adb454c44a8f40c
DevooKim <khwoo8026@gmail.com>
2021-01-21 13:53:24 +0900

    [82] 책풀이 추가

--- week10/book/82devoo.py
+++ week10/book/82devoo.py
@@ -22,5 +22,30 @@ class Solution:
 
         return count
 
+    #그리디
+    def book1(self, g, s):
+        g.sort()
+        s.sort()
+
+        child_i = cookie_j = 0
+
+        while child_i < len(g) and cookie_j < len(s):
+            if s[cookie_j] >= g[child_i]:
+                child_i += 1
+        return child_i
+
+    #이진 탐색
+    def book2(self, g, s):
+        g.sort()
+        s.sort()
+
+        result = 0
+        for i in s:
+            index = bisect.bisect_right(g, i)
+            if index > result:
+                result += 1
+
+        return result
+
 a = Solution()
 print(a.findContentChildren([1,2,3],[1,2,4]))
