1d21aac10a5c7e119f70e5d9b906ded3a2bdb3cb
DevooKim <khwoo8026@gmail.com>
2021-01-21 10:56:10 +0900

    [78] 책풀이 추가

--- week10/book/78devoo.py
+++ week10/book/78devoo.py
@@ -20,6 +20,16 @@ class Solution:
         if buy != -1:
             profit += (prices[-1] - buy)
         return profit
+
+    def book(self, prices):
+        result = 0
+        for i in range(len(prices) - 1):
+            if prices[i + 1] > prices[i]:
+                result += prices[i + 1] - prices[i]
+        return result
+
+    def book2(self, prices):
+        return sum(max(prices[i + 1] - prices[i], 0) for i in range(len(prices) - 1))
 a = Solution()
 # print(a.maxProfit([7,1,5,3,6,4]))
 print(a.maxProfit([1,2,3,4,5]))
