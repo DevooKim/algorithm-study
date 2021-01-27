1708c9e6645c6d1a3fc34ba56c6a2dabba1e5bc9
DevooKim <khwoo8026@gmail.com>
2021-01-27 18:44:58 +0900

    [83] 책풀이 추가

--- week11/book/83devoo.py
+++ week11/book/83devoo.py
@@ -35,5 +35,37 @@ class Solution:
                 result.append(c)
 
         return result
+
+    #브루트 포스
+    def book1(self, nums):
+        for num in nums:
+            if nums.count(num) > len(nums) // 2:
+                return num
+
+    #다이나믹 프로그래밍
+    def book2(self, nums):
+        counts = collections.defaultdict(int)
+        for num in nums:
+            if counts[nums] == 0:
+                counts[nums] = nums.count(num)
+
+            if counts[nums] > len(nums) // 2:
+                return num
+
+    #분할 정복
+    def book3(self, nums):
+        if not nums:
+            return None
+
+        if len(nums) == 1:
+            return nums[0]
+
+        half = len(nums) // 2
+
+        a = self.book3(nums[:half])
+        b = self.book3(nums[half:])
+
+        return [b, a][nums.count(a) > half] #a가 half보다 작으면 [b,a][0]으로 b리턴 => half보다 큰 것만 리턴하게 된다.
+
 a = Solution()
 print(a.majorityElement([2,2,1,1,1,2,2]))
