1f91074bc0bcb348835a8fc6a4a3f952e8f20260
DevooKim <khwoo8026@gmail.com>
2021-01-29 00:59:01 +0900

    [88]책풀이 추가

--- week11/book/88devoo.py
+++ week11/book/88devoo.py
@@ -13,8 +13,21 @@ class Solution:
         for i in range(2, len(nums)):
             nums[i] += nums[i-2]
 
-        print(nums)        
         return max(nums)
+
+    def book(self, nums):
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+
+        dp = collections.OrderedDict()
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp.popitem()[1]
 
 a = Solution()
 print(a.rob([1,2,3,1]))
