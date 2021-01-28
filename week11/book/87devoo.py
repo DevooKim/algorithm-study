b6967b4cc8b4627d69ddf4159a0df5347d71e190
DevooKim <khwoo8026@gmail.com>
2021-01-29 00:36:45 +0900

    [87] 책풀이 추가

--- week11/book/87devoo.py
+++ week11/book/87devoo.py
@@ -8,6 +8,8 @@ import math
 import bisect
 from typing import *
 
+#내풀이 36ms, 14.1MB
+#책풀이 20ms, 14.4MB
 class Solution:
     def climbStairs(self, n: int) -> int:
         a, b = 0, 0 #1계단, 2계단
@@ -18,15 +20,23 @@ class Solution:
         else:
             a, b = 1, (n-1) // 2
 
-        # a += 2
-        # b -= 1
-
         while b:
             l = math.factorial(a + b)
             result += l / (math.factorial(a) * math.factorial(b))
             a += 2
             b -= 1
-        return int(result)
+        return int(result) + 1
+
+    dp = collections.defaultdict(int)
+    def book(self, n):
+        if n <= 2:
+            return n
+        
+        if self.dp[n]:
+            return self.dp[n]
+        self.dp[n] = self.book(n-1) + self.book(n-2)
+        return self.dp[n]
+
 
 
 a = Solution()
