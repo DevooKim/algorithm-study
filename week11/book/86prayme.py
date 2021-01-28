4e3699f10f68f3b69382ed5a397b4e12eabdf1c0
plzprayme <dbfpzk142@gmail.com>
2021-01-28 15:06:16 +0900

    86 책풀이

--- week11/book/86prayme.py
+++ week11/book/86prayme.py
@@ -20,6 +20,21 @@ class Solution:
             window += 1
         return answer
 
+    def memoization(self, nums: List[int]) -> int:
+        # 뒤에서 부터 합한다.
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1] if nums[i - 1] > 0 else 0
+
+    def kadene(self, nums: List[int]) -> int:
+        best_sum = -sys.maxsize
+        current_sum = 0
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            best_sum = max(best_sum, current_sum)
+
+        return best_sum
+
+
 print(Solution().maxSubArray([-2,-1,-3,4,-1,2,1,-5,4])) #6
 print(Solution().maxSubArray([1])) #1
 print(Solution().maxSubArray([0])) #0
